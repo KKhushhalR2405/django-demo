878c14c7f3bc7290eba3e444a2b70477efbb9d78
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-09 23:06:03 +0530

    sample django pp

---
+++ demo/home/views.py
@@ -0,0 +1,22 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def index(request):
+    
+    return render(request,'index.html')
+    # return HttpResponse("This is home page")
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def services(request):
+    return render(request,'services.html')
+
