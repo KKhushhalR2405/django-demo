a196cf7384ce518820fc30e8bb36a11adbe15b82
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-12 21:20:32 +0530

    added db suport

--- demo/home/views.py
+++ demo/home/views.py
@@ -1,5 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 
@@ -14,6 +16,15 @@ def about(request):
 
 
 def contact(request):
+    if request.method=='POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+
+        contact = Contact(name = name, email = email, phone=phone, desc = desc, date = datetime.today())
+        contact.save()
+
     return render(request,'contact.html')
 
 
